4c52c2ac9cb34743ba90f3f4d9dcb36b081955b9
kyrosle <le@90e.com>
2022-11-22 22:18:06 +0800

    fix upload

--- src/components/add_page/add_regulations.rs
+++ src/components/add_page/add_regulations.rs
@@ -1,8 +1,23 @@
-use backend::display_department_information;
+use backend::{
+    display_attendance_type_information, display_department_information,
+    display_political_information, display_post_information,
+};
 use dioxus::{events::FormEvent, prelude::*};
 
+macro_rules! display_vec {
+    ($func: ident, $show_table: expr) => {
+        let vec = $func()
+            .await
+            .unwrap()
+            .into_iter()
+            .map(|v| v.name.unwrap())
+            .collect::<Vec<_>>();
+        $show_table.set(vec);
+    };
+}
+
 pub(crate) fn AddRegulations(cx: Scope) -> Element {
-    let choices = vec!["Department", "Post", "Employee Change", "Political"]
+    let choices = vec!["Department", "Post", "Attendance Type", "Political Type"]
         .into_iter()
         .map(|s| s.to_string())
         .collect::<Vec<_>>();
@@ -11,26 +26,34 @@ pub(crate) fn AddRegulations(cx: Scope) -> Element {
             option { "{s}" }
         }
     });
-    let show_table = use_state(&cx, || vec![]);
+    let show_table = use_state(&cx, std::vec::Vec::new);
 
     let show_button = move |e: FormEvent| {
-        cx.spawn(async move {
-            match &e.values["show"] {
-                s if s == "Department" => {
-                    let department_vec = display_department_information()
-                        .await
-                        .unwrap()
-                        .into_iter()
-                        .map(|d| d.name.unwrap())
-                        .collect::<Vec<_>>();
-                    // println!("{:#?}", department_vec);  -has value-
-                    show_table.set(department_vec);
-                }
-                s if s == "Post" => {}
-                s if s == "Employee Change" => {}
-                s if s == "Salary" => {}
+        cx.spawn({
+            let show_table = show_table.clone();
+            async move {
+                match &e.values["show"] {
+                    s if s == "Department" => {
+                        display_vec!(display_department_information, show_table);
+                    }
+                    s if s == "Post" => {
+                        display_vec!(display_post_information, show_table);
+                    }
+                    s if s == "Attendance Type" => {
+                        let attendance_vec = display_attendance_type_information()
+                            .await
+                            .unwrap()
+                            .into_iter()
+                            .map(|v| v.attendance_name.unwrap())
+                            .collect::<Vec<_>>();
+                        show_table.set(attendance_vec);
+                    }
+                    s if s == "Political Type" => {
+                        display_vec!(display_political_information, show_table);
+                    }
 
-                _ => {}
+                    _ => {}
+                };
             }
         })
     };
@@ -64,6 +87,15 @@ pub(crate) fn AddRegulations(cx: Scope) -> Element {
                             "Show"
                         }
                     }
+                }
+            }
+            table {
+                class: "table",
+                thead {
+                    tr { th { title: "Name", "Name" } }
+                }
+                tbody {
+                    show_table.get().iter().map(|s| rsx!(tr { th {"{s}"} }))
                 }
             }
         }
